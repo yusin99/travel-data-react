4b308de8d37a4212dcd1e66bcf93f533f8639cad
Yusin <sunkatabeckama@gmail.com>
2022-01-23 18:51:09 +0100

    Setting up google developer profile + Free trial 300$ API requests in order to use the API provided from Google + Configuring the layout of the MAP side of the project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
         <Typography variant="h5" className={classes.title}>
-          Travel Advisor
+          Travel Data
         </Typography>
         <Box display="flex">
           <Typography variant="h6" className={classes.title}>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { LocationOnOutlined } from "@material-ui/icons";
+import { Rating } from "@material-ui/lab";
+import GoogleMapReact from "google-map-react";
+import useStyles from "./styles";
 const Map = () => {
-  return <div>Map</div>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 0, lng: 0 };
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyDBNQfLUB2PjiimmcfVY6aZBf41VtHkLCM" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
